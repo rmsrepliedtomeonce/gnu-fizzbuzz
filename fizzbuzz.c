ecd6fc24d230eb3138690a1e7e0ae5fdf7eb516d
Adam Recende <gnulinuxorocks45@myself.com>
2013-09-14 21:33:05 +0300

    v1.00  - initial release v1.01  - fixed arrangement v4.2.0-r1  - fixed the infamous bug where buzz disguised itself as fizz

---
+++ fizzbuzz.c
@@ -0,0 +1,33 @@
+  //© Copyright 2013 Adam, <gnulinuxorocks45 at myself.com>. All Rights Reserved.
+  //This program is free software: you can redistribute it and/or modify
+  // it under the terms of the GNU General Public License as published by
+  // the Free Software Foundation, either version 3 of the License, or
+  //  (at your option) any later version.
+  //
+  // This program is distributed in the hope that it will be useful,
+  // but WITHOUT ANY WARRANTY; without even the implied warranty of
+  // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  // GNU General Public License for more details.
+  //
+  // You should have received a copy of the GNU General Public License
+  // along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdio.h>
+
+int main() {
+
+  int num;
+
+  for(num=1;num!=100;num++) {
+    if(num%15 == 0) {
+      printf("FizzBuzz.\n");
+    } else if(num%5 == 0) {
+      printf("Buzz.\n");
+    } else if(num%3 == 0) {
+      printf("Fizz.\n");
+    } else {
+      printf("%i\n", num);
+    }
+  }
+  return 0;
+}
